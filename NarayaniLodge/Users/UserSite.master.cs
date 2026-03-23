cfaa311eb7c98df6c6ba5a03dcbb4cd604a514b9
Diksha Futane <diksha@gmail.com>
2026-03-23 14:41:26 +0530

    Users Master page

---
+++ NarayaniLodge/Users/UserSite.master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Users_UserSite : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["UserEmail"] == null)
+        {
+            Response.Redirect("~/Authentication/Login.aspx");
+        }
+
+        if (!IsPostBack)
+        {
+            lblUserName.Text = Session["UserName"].ToString();
+        }
+
+    }
+}
